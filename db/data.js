5fd4ae1386273eec20bdd64ac4a8911600d3254b
Andy <blold@163.com>
2021-12-12 23:04:13 +0800

    init function for html CRUD

---
+++ db/data.js
@@ -0,0 +1,24 @@
+var dbData = {
+  "dataList": [
+    {
+      "no": "1",
+      "studyNo": "001",
+      "name": "小王",
+      "college": "信息",
+      "major": "软件",
+      "grade": "1",
+      "class": "班级1",
+      "age": "20"
+    },
+    {
+      "no": "2",
+      "studyNo": "002",
+      "name": "小李",
+      "college": "信息",
+      "major": "电子",
+      "grade": "1",
+      "class": "班级2",
+      "age": "20"
+    }
+  ]
+}
